ca69bbec609449eda511bde891ce1d9f861f6aee
drago <97@dragonaf.eu>
2024-07-01 13:47:42 +0200

    alte Bilder werden beim updaten und löschen der location nun gelöscht

--- src/routes/locations.js
+++ src/routes/locations.js
@@ -3,6 +3,7 @@ let router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const mongoCRUDs = require('../db/mongoCRUDs');
+const fs = require('fs')
 
 // Multer Setup
 const storage = multer.diskStorage({
@@ -51,12 +52,40 @@ router.get('/:id', async function (req, res) {
 router.put('/:id', upload.single('image'), async function (req, res) {
     try {
         const updatedLocation = req.body;
-        if (req.file) {
-            updatedLocation.image = '/uploads/' + req.file.filename;
+        const existingLocation = await mongoCRUDs.getLocationById(req.params.id);
+
+        if (existingLocation) {
+            if (req.file) {
+                // Delete the old image file if a new one is uploaded
+                if (existingLocation.image) {
+                    const oldImagePath = path.join(__dirname, '../../public', existingLocation.image);
+                    console.log('Trying to delete old image file at:', oldImagePath);
+                    fs.access(oldImagePath, fs.constants.F_OK, (err) => {
+                        if (!err) {
+                            fs.unlink(oldImagePath, (err) => {
+                                if (err) {
+                                    console.error('Failed to delete old image file:', err);
+                                } else {
+                                    console.log('Successfully deleted old image file:', oldImagePath);
+                                }
+                            });
+                        } else {
+                            console.error('Old image file does not exist:', oldImagePath);
+                        }
+                    });
+                }
+                updatedLocation.image = '/uploads/' + req.file.filename;
+            } else {
+                // Keep the old image if no new one is uploaded
+                updatedLocation.image = existingLocation.image;
+            }
+
+            const updatedCount = await mongoCRUDs.updateLocation(req.params.id, updatedLocation);
+            if (updatedCount > 0) res.status(204).send();
+            else res.status(404).send('Location not found');
+        } else {
+            res.status(404).send('Location not found');
         }
-        const updatedCount = await mongoCRUDs.updateLocation(req.params.id, updatedLocation);
-        if (updatedCount > 0) res.status(204).send();
-        else res.status(404).send('Location not found');
     } catch (err) {
         res.status(500).send('Error updating location');
     }
@@ -64,9 +93,35 @@ router.put('/:id', upload.single('image'), async function (req, res) {
 
 router.delete('/:id', async function (req, res) {
     try {
-        const deletedCount = await mongoCRUDs.deleteLocation(req.params.id);
-        if (deletedCount > 0) res.status(204).send();
-        else res.status(404).send('Location not found');
+        const location = await mongoCRUDs.getLocationById(req.params.id);
+        if (location) {
+            // Delete the image file if it exists
+            if (location.image) {
+                const imagePath = path.join(__dirname, '../../public', location.image);
+                console.log('Trying to delete image file at:', imagePath);
+                fs.access(imagePath, fs.constants.F_OK, (err) => {
+                    if (!err) {
+                        fs.unlink(imagePath, (err) => {
+                            if (err) {
+                                console.error('Failed to delete image file:', err);
+                            } else {
+                                console.log('Successfully deleted image file:', imagePath);
+                            }
+                        });
+                    } else {
+                        console.error('Image file does not exist:', imagePath);
+                    }
+                });
+            }
+            const deletedCount = await mongoCRUDs.deleteLocation(req.params.id);
+            if (deletedCount > 0) {
+                res.status(204).send();
+            } else {
+                res.status(404).send('Location not found');
+            }
+        } else {
+            res.status(404).send('Location not found');
+        }
     } catch (err) {
         res.status(500).send('Error deleting location');
     }
