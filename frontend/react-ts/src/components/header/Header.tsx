411ad6c928700187deb36a75aa5553bf8dc3ca2c
Georg.Schwachula <georg.schwachula@student.htw-berlin.de>
2025-05-17 15:52:25 +0200

    refinement

---
+++ frontend/react-ts/src/components/header/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+        <header className="header bg-blue-700 text-white p-4 flex items-center justify-center space-x-4">
+            <img src="/logo.svg" alt="App Logo" className="h-10 w-10" />
+            <h1 className="text-2xl font-bold">Green Berlin</h1>
+        </header>
+    );
+}
